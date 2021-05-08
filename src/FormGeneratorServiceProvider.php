82e574d8acfbd09157c8a5605bf8cbc6b603a5cf
Jhonny Fialho <jhonny@hello-world.cc>
2021-05-08 18:15:16 -0300

    first commit

---
+++ src/FormGeneratorServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace SenventhCode\FormGenerator;
+
+use Illuminate\Support\ServiceProvider;
+
+class FormGeneratorServiceProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+        
+    }
+
+    public function register()
+    {
+       
+    }
+}
