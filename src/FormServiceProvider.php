b18b708c372318e8669affa1941439fe1c418ab8
Jhonny Fialho <jhonny@hello-world.cc>
2021-05-11 22:11:54 -0300

    teste

---
+++ src/FormServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace DevinGray\CustomAuth\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class FormServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register ()
+    {
+        # code...
+    }
+}
