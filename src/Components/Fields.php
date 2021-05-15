0b3c8e9fce765607340ce722cdac1b5e9d7db2c5
Jhonny Fialho <jhonny@hello-world.cc>
2021-05-15 01:03:05 -0300

    ajustes de views

--- src/Components/Fields.php
+++ src/Components/Fields.php
@@ -24,6 +24,6 @@ class Fields extends Component
 
     public function render()
     {
-        return view('components.form-fields');
+        return view('form-service::components.fields');
     }
 }
